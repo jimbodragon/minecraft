48680c347d7b0d509fcd2310da6ac6e57142f65b
Jimmy Provencher <jimmy.provencher@hotmail.ca>
2021-11-30 08:43:38 -0500

    Set default version to 0.0.0

--- install_minecraft_server.sh
+++ install_minecraft_server.sh
@@ -4,6 +4,7 @@ minecraft_home="$minecraft_folder"
 
 function create_start()
 {
+	echo "Creating start script"
 	minecraftname="$1"
 	rconpassword="$2"
 	server_version="$3"
@@ -21,18 +22,37 @@ function check_version_and_download()
 	use_forge="$use_forge"
 	minecraft_folder="$minecraft_folder"
 
-	if [ "\$2" == ""]
+	newer_version="\$1"
+	actual_version="\$2"
+	downloadlink="\$3"
+	desire_version="\$4"
+
+	if [ "\$actual_version" != "" ]
 	then
-		if [ \$1 > \$2 ]
+		if [ \$newer_version -gt \$actual_version ]
 		then
-			wget -O $minecraft_folder/server/server.jar \$3
-			echo "\$4" > $minecraft_folder/server/server.version
+			wget -O $minecraft_folder/server/server.jar \$downloadlink
+			echo "Create version file at \$desire_version"
+			echo "\$desire_version" > $minecraft_folder/server/server.version
 		fi
+	fi
+
+	if [ "\$newer_version" == "" ]
+	then
+		echo "Create version file at \$desire_version"
+		echo "0.0.0" > \$minecraft_folder/server/server.version
+	elif [ "\$newer_version" != "" ]
+	then
+		echo "\$newer_version" > \$minecraft_folder/server/server.version
+	else
+		echo "0.0.0" > \$minecraft_folder/server/server.version
+		echo "\$server_version" > \$minecraft_folder/server/server.version
 	fi
 }
 
 function check_update()
 {
+	echo "Cehcking static or latest update"
 	server_version="$server_version"
 	use_forge="$use_forge"
 	minecraft_folder="$minecraft_folder"
@@ -46,12 +66,15 @@ function check_update()
 
 	if [ "\$server_version" == "latest" ] && [ "\$use_forge" != "true" ]
 	then
-		latest_version=\$(jq .latest.release "\$minecraft_folder/version_manifest.json")
+		echo "Wow"
+		latest_version=\$(jq .latest.release "$minecraft_folder/version_manifest.json")
 	elif [ "\$server_version" != "latest" ]
+	then
 		latest_version=\$server_version
 	fi
 
-	downloadlink="\$(wget -O - "$(jq ".versions[] | select(.id == \\\"\$latest_version\\\")" "\$minecraft_folder/version_manifest.json" | jq .url | cut -d '\"' -f 2)" | jq .downloads.server.url | cut -d '\"' -f 2)"
+		echo "Yahoo"
+	downloadlink="\$(wget -O - "\$(jq ".versions[] | select(.id == \"\$latest_version\")" "$minecraft_folder/version_manifest.json" | jq .url | cut -d '"' -f 2)" | jq .downloads.server.url | cut -d '"' -f 2)"
 
 	latest_major_version=\$(echo "\$latest_version" | cut -d '.' -f 1)
 	latest_minor_version=\$(echo "\$latest_version" | cut -d '.' -f 2)
@@ -72,14 +95,15 @@ function check_update()
 
 function check_server()
 {
+	echo "Setting in place the minecraft server configuration"
 	server_version="$server_version"
 	use_forge="$use_forge"
 	minecraft_folder="$minecraft_folder"
+	levelseed="$levelseed"
 
 	if [ ! -f \$minecraft_folder/server/eula.txt ]
 	then
 		echo -e "#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).\neula=true" > \$minecraft_folder/server/eula.txt
-
 	fi
 
 	if [ ! -f \$minecraft_folder/server/server.properties ]
@@ -130,17 +154,17 @@ enable-rcon=true
 motd=La Communaute de \$minecraftname
 #level-seed=
 EOF
-		if [ "\$1" == ""]
+		if [ "q$levelseed" != "q" ]
 		then
-			echo "\$1" > \$minecraft_folder/server/server.version
-		else
-			echo "0.0.0" > \$minecraft_folder/server/server.version
+				echo 'tatata'
+				# sed -i 's|#level-seed=|level-seed=$levelseed|g' "\$minecraft_folder/server/server.properties"
 		fi
 	fi
 }
 
 function check_mount()
 {
+	echo "mounting the ram fs"
 	minecraft_folder="$minecraft_folder"
 	num_mount=\$(df -h | grep $minecraft_folder/server | wc -l)
 	if [ \$num_mount -eq 0 ]
@@ -154,9 +178,12 @@ function check_mount()
 
 function revert_backup()
 {
+	echo "Extracting last backup"
+	minecraftname="$minecraftname"
 	minecraft_folder="$minecraft_folder"
+
   cd $minecraft_folder/server
-  backup_file="$minecraft_folder/backups/server-$1-minecraft.tar.gz"
+  backup_file="$minecraft_folder/backups/server-$minecraftname-minecraft.tar.gz"
 	if [ -f \$backup_file ]
 	then
 		tar -xzf \$backup_file
@@ -183,7 +210,7 @@ EOS
 
 function create_backup_tool()
 {
-	$minecraftname="$1"
+	minecraftname="$1"
 	rconpassword="$2"
 	dropboxserver="$3"
 	dropboxuser="$4"
@@ -248,21 +275,28 @@ function validate_parameter()
 	message="$3"
 	require="$4"
 	parameter_value="$(eval "echo \$$parameter_name")"
+	buffer_minecraftname="$minecraftname"
 
 	if [ "$require" == "true" ]
 	then
-		minecraftname="$default_value"
+		buffer_minecraftname="$default_value"
+		echo "Setting buffer_minecraftname to generic name '$buffer_minecraftname'"
 	fi
 
 	if [ "$parameter_value" == "" ]
 	then
-		read -p "$minecraftname -> $message: " $parameter_name
+		read -p "$buffer_minecraftname -> $message: " $parameter_name
 		parameter_value="$(eval "echo \$$parameter_name")"
-	fi
 
-	if [ "$parameter_value" == "" ]
-	then
-		$parameter_value="$default_value"
+		if [ "$parameter_value" == "" ]
+		then
+			export $parameter_name="$default_value"
+			parameter_value="$(eval "echo \$$parameter_name")"
+			echo "Parameter $parameter_name ('$parameter_value') set to default value ('$default_value')"
+		else
+			# export $parameter_name="$parameter_value"
+			echo "Parameter $parameter_name correctly set to '$parameter_value'"
+		fi
 	fi
 
 	if [ "$require" == "true" ] && [ "$parameter_value" == "" ]
@@ -270,9 +304,13 @@ function validate_parameter()
 		echo "parameter $parameter_name is require"
 		echo "Exiting"
 		exit 1
+	elif [ "$require" == "false" ]
+	then
+		echo "No default or enter parameter for a non require"
+		echo "Continuing"
 	elif [ "$parameter_value" == "" ]
 	then
-		echo "not require parameter $parameter_name is nil"
+		echo "not require parameter $parameter_name is nil (default at '$default_value')"
 		echo "Exiting"
 		exit 2
 	fi
@@ -311,15 +349,17 @@ EOS
 
 }
 
+default_minecraftname="Minecraft"
 default_server_version="latest"
 default_use_forge="false"
+default_server_version="1.17.1"
 default_mcron_version="0.7.2"
 default_minecraft_folder="/opt/minecraft"
-default_openjdk_version="18"
+default_openjdk_version="17"
 default_minecraft_user="minecraft"
 default_fs_ram_mb="4096"
 
-validate_parameter minecraftname "Minecraft" "Minecraft Instance name" true
+validate_parameter minecraftname "$default_minecraftname" "Minecraft Instance name" true
 validate_parameter rconpassword "$minecraftname" "Minecraft mcrcon password" true
 validate_parameter server_version "$default_server_version" "server version (empty for $default_server_version)"
 validate_parameter use_forge "$default_use_forge" "use forge server (true to activate)"
@@ -331,18 +371,33 @@ validate_parameter mcron_version "$default_mcron_version" "mcron_version (empty 
 
 validate_parameter fs_ram_mb "$default_fs_ram_mb" "size of ram fs for minecraft folder (empty for $default_fs_ram_mb)"
 
-validate_parameter levelseed "" "level seed"
+validate_parameter levelseed "" "level seed" false
 
-validate_parameter dropboxserver "" "Dropbox server for backup"
-validate_parameter dropboxuser "" "Dropbox user for backup"
+validate_parameter dropboxserver "" "Dropbox server for backup" false
+validate_parameter dropboxuser "" "Dropbox user for backup" false
 
-backuplog="$minecraft_folder/log/$minecraftname.backup.log"
+backuplogs="$minecraft_folder/logs/$minecraftname.backup.logs"
 
-echo -e "minecraftname = $minecraftname\nrconpassword = $rconpassword\ndropboxserver = $dropboxserver\ndropboxuser = $dropboxuser\nlevelseed = $levelseed\nserver_version = $server_version\nuse_forge = $use_forge\nmcron_version = $mcron_version\nminecraft_folder = $minecraft_folder\nopenjdk_version = $openjdk_version\nbackuplog = $backuplog"
+echo -e "minecraftname = $minecraftname\nrconpassword = $rconpassword\ndropboxserver = $dropboxserver\ndropboxuser = $dropboxuser\nlevelseed = $levelseed\nserver_version = $server_version\nuse_forge = $use_forge\nmcron_version = $mcron_version\nminecraft_folder = $minecraft_folder\nopenjdk_version = $openjdk_version\nbackuplogs = $backuplogs"
 
-apt-get install "openjdk-$openjdk_version-jre-headless" "htop" "sudo" "net-tools"
+echo "Testing connectivity on $dropboxserver"
+nc -z $dropboxserver
+code="$?"
+if [ "$code" == "0" ]
+then
+	echo "Enter password for $dropboxuser@$dropboxserver"
+	su - $minecraft_user -c "/usr/bin/ssh-keygen -q -t rsa -f $minecraft_folder/.ssh/id_rsa -P ''"
+	sshkey=$(cat $minecraft_folder/.ssh/id_rsa.pub)
+	ssh $dropboxuser@$dropboxserver "echo '$sshkey' >> .ssh/authorized_keys"
+fi
+
+apt-get install -y "openjdk-$openjdk_version-jre-headless" "htop" "sudo" "net-tools" "jq"
 useradd -r -m -U -d $minecraft_folder -s /bin/bash $minecraft_user
-echo -e "tmpfs       $minecraft_folder/server/ tmpfs   nodev,nosuid,noexec,nodiratime,size=$default_fs_ram_mb\M   0 0" >> /etc/fstab
+
+if [ "$(grep "$minecraft_folder/server/" /etc/fstab)" == "" ]
+then
+	echo -e "tmpfs       $minecraft_folder/server/ tmpfs   nodev,nosuid,noexec,nodiratime,size=$default_fs_ram_mb\M   0 0" >> /etc/fstab
+fi
 
 mkdir -p $minecraft_folder/{backups,tools,server,logs}
 wget -O "$minecraft_folder/tools/mcrcon-$mcron_version-linux-x86-64.tar.gz" "https://github.com/Tiiffi/mcrcon/releases/download/v$mcron_version/mcrcon-$mcron_version-linux-x86-64.tar.gz"
@@ -351,23 +406,32 @@ rm "$minecraft_folder/tools/mcrcon-$mcron_version-linux-x86-64.tar.gz"
 find -name mcrcon -exec cp {} $minecraft_folder/tools/ \;
 find / -type d -name "mcrcon*" -exec rm -rf {} \;
 
-echo "4,9,14,19,24,29,34,39,44,49,54,59 * * * * $minecraft_user $minecraft_folder/tools/backup.sh > \"$backuplog\"" > /etc/cron.d/$minecraftname_backup
-echo > "$backuplog"
-chown $minecraft_user:$minecraft_user "$backuplog"
+echo "4,9,14,19,24,29,34,39,44,49,54,59 * * * * $minecraft_user $minecraft_folder/tools/backup.sh > \"$backuplogs/$minecraftname.logs\"" > "/etc/cron.d/$minecraftname\_backup"
+echo > "$backuplogs"
+chown $minecraft_user:$minecraft_user "$backuplogs"
 echo "$minecraft_user ALL=(root) NOPASSWD: /usr/bin/mount $minecraft_folder/server" > /etc/sudoers.d/$minecraft_user
 
+echo "Create version file at 0.0.0"
+echo "0.0.0" > $minecraft_folder/server/server.version
+
+echo "Start create_start"
 create_start "$minecraftname" "$rconpassword" "$server_version" "$use_forge" "$minecraft_folder" "$minecraft_user" "$levelseed" "$fs_ram_mb"
+echo "End create_start"
+echo "Start create_backup_tool"
 create_backup_tool "$minecraftname" "$rconpassword" "$dropboxserver" "$dropboxuser"
+echo "End create_backup_tool"
+echo "Start create_service"
 create_service "$minecraftname" "$rconpassword"
+echo "End create_service"
+echo "Start create_response"
 create_response "$minecraftname" "$rconpassword" "$server_version" "$use_forge" "$minecraft_folder" "$minecraft_user" "$openjdk_version" "$mcron_version" "$fs_ram_mb" "$levelseed" "$dropboxserver" "$dropboxuser"
+echo "End create_response"
 
 chmod -R ug+x $minecraft_folder/tools/*.sh
 chown -R $minecraft_user:$minecraft_user $minecraft_folder/
 
-echo "Enter password for $dropboxuser@$dropboxserver"
-su - $minecraft_user -c "/usr/bin/ssh-keygen -q -t rsa -f $minecraft_folder/.ssh/id_rsa -P ''"
-sshkey=$(cat $minecraft_folder/.ssh/id_rsa.pub)
-ssh $dropboxuser@$dropboxserver "echo '$sshkey' >> .ssh/authorized_keys"
 
 systemctl daemon-reload
 systemctl start $minecraftname.service
+sleep 3
+systemctl status $minecraftname.service
